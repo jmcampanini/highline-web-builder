cd517446f122e9cadd6f34dd111ac62994546009
Javier Campanini <jcampanini@Javiers-MacBook-Pro.local>
2015-01-16 01:35:02 -0500

    tweaked the build process a bit.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install gulp and the rest of the npm plugins
-./gradlew npmInstall
-./gradlew installGulp
+./gradlew npmInstall installGulp
 
+# run a dummy command from gradle
 ./gradlew gulp_helloworld
