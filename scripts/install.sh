3de3f257bef7fd1e9cfdb58a9e312d17804dc1fe
Javier Campanini <jcampanini@Javiers-MacBook-Pro.local>
2015-01-18 03:56:48 -0500

    moved the cleaning commands out of the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,6 @@
 set -e
 set -x
 
-echo "\nClearing out old files...\n"
-rm -rf build/
-rm -rf .node/
-rm -rf .gradle/
-rm -rf node_modules/
-
 echo "\nInstalling Gulp Globally...\n"
 npm install --global gulp
 
